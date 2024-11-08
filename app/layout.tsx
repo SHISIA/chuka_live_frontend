9e27579f6711c5e1364d3b1c9497a51d3829c9a2
Jemo <jamesshisia6@gmail.com>
2024-11-08 12:20:55 +0300

    set up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -28,8 +28,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AppRouterCacheProvider>
+          {children}
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
 }
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        goldenYellow: '#F2BB47',
+        textThemeBrown:"#653A3A"
       },
     },
   },
